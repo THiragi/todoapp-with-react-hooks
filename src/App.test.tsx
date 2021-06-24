d18178f34eb101afccf95171315885406bfadf2f
THiragi <w1th1nmybr41n@gmail.com>
2021-06-24 23:38:50 +0900

    update: App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('renders App Component', async () => {
+    render(<App />);
+
+    // この時点ではまだ"Study in TypeScript"タスクはない
+    expect(screen.queryByText(/Study in TypeScript/)).toBeNull();
+    // フォームに文字を入力
+    userEvent.type(screen.getByRole('textbox'), 'Study in TypeScript');
+    // 追加ボタンをクリック
+    userEvent.click(screen.getByText('追加'));
+    // タスクが追加されている
+    expect(screen.getByText(/Study in TypeScript/)).toBeInTheDocument();
+  });
 });
